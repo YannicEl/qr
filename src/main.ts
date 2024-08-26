a3469359a6ca577d9df0a5f02558e0260db6e9ee
Yannic <me@yannic.at>
2024-08-26 18:29:24 +0200

    done

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,66 @@
 import './style.css';
 import encodeQR from '@paulmillr/qr';
 
-const app = document.querySelector<HTMLDivElement>('#app');
-const formElm = document.querySelector<HTMLFormElement>('#generateForm');
-const qrImgElm = document.querySelector<HTMLImageElement>('#qr');
+const formElm = document.querySelector<HTMLFormElement>('#generateForm')!;
+const qrImgElm = document.querySelector<HTMLImageElement>('#qrImg')!;
+const donwloadButtonsElm = document.querySelector<HTMLImageElement>('#downloadButtons')!;
 
-if (app && formElm && qrImgElm) {
-	formElm.onsubmit = (event) => {
-		event.preventDefault();
+const canvas = document.querySelector<HTMLCanvasElement>('#canvas')!;
 
-		const formData = new FormData(formElm);
+formElm.onsubmit = async (event) => {
+	event.preventDefault();
 
-		const url = formData.get('url');
-		if (typeof url !== 'string') return;
+	const formData = new FormData(formElm);
 
-		qrImgElm.src = urlToSvgDataUrl(url);
-	};
+	const url = formData.get('url');
+	if (typeof url !== 'string') return;
+
+	qrImgElm.src = await urlToSvgDataUrl(url);
+	qrImgElm.style.display = 'block';
+	donwloadButtonsElm.style.opacity = '1';
+};
+
+const svgDownloadButtonElm = document.querySelector<HTMLButtonElement>('#svgDownload')!;
+svgDownloadButtonElm.onclick = (event) => {
+	event.preventDefault();
+	downloadAsSvg();
+};
+
+function downloadAsSvg() {
+	downloadFile(qrImgElm.src, 'qr.svg');
 }
 
-function urlToSvgDataUrl(url: string): string {
-	const svgString = encodeQR(url, 'svg');
+const pngDownloadButtonElm = document.querySelector<HTMLButtonElement>('#pngDownload')!;
+pngDownloadButtonElm.onclick = (event) => {
+	event.preventDefault();
+	downloadAsPng();
+};
+
+function downloadAsPng() {
+	const canvas = document.createElement('canvas');
+	canvas.width = 3000;
+	canvas.height = 3000;
+	const ctx = canvas.getContext('2d')!;
+	ctx.drawImage(qrImgElm, 0, 0, 3000, 3000);
+
+	const dataUri = canvas.toDataURL('image/png');
+	downloadFile(dataUri, 'qr.png');
+}
+
+function downloadFile(dataUri: string, filename: string) {
+	const link = document.createElement('a');
+	link.download = filename;
+	link.href = dataUri;
+
+	document.body.appendChild(link);
+	link.click();
+	document.body.removeChild(link);
+}
+
+async function urlToSvgDataUrl(url: string): Promise<string> {
+	const svgString = encodeQR(url, 'svg', {
+		border: 0,
+	});
 	const svgElement = new DOMParser().parseFromString(svgString, 'text/xml');
 	const xml = new XMLSerializer().serializeToString(svgElement);
 	const src = `data:image/svg+xml;charset=utf-8,${xml}`;
