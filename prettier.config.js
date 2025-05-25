17b1ff2868a749d91a06797ad914b151c34d2151
Yannic <me@yannic.at>
2025-05-25 12:18:49 +0200

    fix

--- prettier.config.js
+++ prettier.config.js
@@ -5,12 +5,6 @@ const config = {
 	singleQuote: true,
 	printWidth: 100,
 	trailingComma: 'es5',
-	plugins: [
-		'prettier-plugin-svelte',
-		'prettier-plugin-organize-imports',
-		'prettier-plugin-tailwindcss',
-	],
-	overrides: [{ files: '*.svelte', options: { parser: 'svelte' } }],
 };
 
 export default config;
